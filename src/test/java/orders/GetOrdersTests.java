c3eed8f162ec1bedd9acc353dc8850737ec97dca
Nikolay <alstrongnik@gmail.com>
2021-11-28 12:01:56 +0300

    Create new tests and allure report

---
+++ src/test/java/orders/GetOrdersTests.java
@@ -0,0 +1,25 @@
+package orders;
+
+import initialization.MainTests;
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.Response;
+import org.junit.Test;
+import rest.ScooterRestUrl;
+
+import static org.hamcrest.Matchers.notNullValue;
+
+public class GetOrdersTests extends MainTests {
+
+    @Test
+    @DisplayName("Check all orders tests of /api/v1/orders")
+    @Description("Basic success test for /api/v1/orders")
+    public void checkAllOrdersTest() {
+
+        Response responseOrder = restScooterSend.getRequestAndReturnResponse(ScooterRestUrl.ORDER_URL);
+
+        responseOrder.then().assertThat()
+                .statusCode(200)
+                .and().body("orders", notNullValue());
+    }
+}
